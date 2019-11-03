49eba3f80f4b22621097c1289f666522d354a88a
Melody-Ruth <melody@sd-fun.org>
2019-11-03 13:01:20 -0800

    testing cave flood

---
+++ testingGithub.js
@@ -0,0 +1,15 @@
+/*function preload() {
+	crystal = loadImage("crystal.png", pic => print(pic));
+};/
+
+function setup() {
+	createCanvas(600,600);
+	noFill();
+	noStroke();
+	background(2, 130, 194); //pick a color
+};
+
+draw = function() {
+	fill(255,0,0);
+	ellipse(50,50,100,100);
+};
